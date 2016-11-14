f45cb665df21966d96e4c3b5e90a2667fc6e1459
Stefan <nimmneun@gmail.com>
2016-11-14 21:44:46 +0100

    Split into nasty looking switch case and included conjured items. Looking so ugly ...

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -24,52 +24,45 @@ class GildedRose
 
     public function tick()
     {
-        if ($this->name == 'Sulfuras, Hand of Ragnaros') {
-            return;
-        }
+        switch ($this->name) {
+            case 'Sulfuras, Hand of Ragnaros':
+                break;
 
-        // aged bree logic
-        if ($this->name == 'Aged Brie') {
-
-            $this->raiseQuality();
-            $this->sellIn--;
-
-            if ($this->sellIn < 0) {
+            case 'Aged Brie':
                 $this->raiseQuality();
-            }
+                $this->sellIn--;
+                if ($this->sellIn < 0) {
+                    $this->raiseQuality();
+                }
+                break;
 
-            return;
-        }
+            case 'Backstage passes to a TAFKAL80ETC concert':
+                $this->raiseQuality();
 
-        // backstage pass logic
-        if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
-            $this->raiseQuality();
-
-            if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($this->sellIn < 11) {
                     $this->raiseQuality();
                 }
+
                 if ($this->sellIn < 6) {
                     $this->raiseQuality();
                 }
-            }
 
-            $this->sellIn--;
+                $this->sellIn--;
 
-            if ($this->sellIn < 0) {
-                $this->quality = 0;
-            }
+                if ($this->sellIn < 0) {
+                    $this->quality = 0;
+                }
+                break;
 
-            return;
-        }
+            default:
+                $this->lowerQuality();
 
-        // entire default item logic
-        $this->lowerQuality();
+                $this->sellIn--;
 
-        $this->sellIn--;
+                if ($this->sellIn < 0) {
+                    $this->lowerQuality();
+                }
 
-        if ($this->sellIn < 0) {
-            $this->lowerQuality();
         }
     }
 
@@ -83,6 +76,12 @@ class GildedRose
     private function lowerQuality()
     {
         if ($this->quality > 0) {
+            $this->quality--;
+        }
+
+        if ($this->quality > 0
+            && 0 === strpos($this->name, 'Conjured')
+        ) {
             $this->quality--;
         }
     }
